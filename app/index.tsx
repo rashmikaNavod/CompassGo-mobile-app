ceafee1578c8e1cbb69996693e86a5e61ce65ec3
rashmikaNavod <drashmikanavod@gmail.com>
2025-09-04 09:51:59 +0530

    add file structure

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+//නමෝ බුද්ධාය
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+
+const App = () => {
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text className="text-xl font-bold text-blue-500">නමෝ බුද්ධාය</Text>
+		</View>
+	);
+};
+
+export default App;
